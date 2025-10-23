25c0efdc43cfbcd5bd94008bf654f1c725c23197
Dzakhwan Aufa Saif <dzakhwanaufa2411@gmail.com>
2025-10-23 15:23:21 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,23 @@
+print("Program Menghitung Luas Bangun Datar")
+print("1. Persegi")
+print("2. Lingkaran")
+print("3. Segitiga")
+
+hitung = int(input("memilih program 1/2/3: "))
+
+match hitung:
+	case 1:
+		sisi = float(input("Masukan panjang sisi: "))
+		luas = sisi * sisi
+		print(luas)
+	case 2:
+		r = float(input("Masukan jari-jari lingkaran: "))
+		luas = 3.14 * r * r
+		print(luas)
+	case 3:
+		alas = float(input("Masukan alas segituga: "))
+		tinggi = float(input("Masukan tinggi segitiga: "))
+		luas = 0.5 * alas * tinggi
+		print(luas)
+	case _:
+		print("SALAH PILIH!")
